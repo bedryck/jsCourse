6633bc5fab42c238732a24e9c0ff4b3398455f70
Рома Коваль <romariooo27@gmail.com>
2019-09-29 21:25:02 +0300

    home work 1 ready

--- lessons/lesson1.js
+++
@@ -1,5 +0,0 @@
-const lessons1 = () =>{
-    console.group('lesson1');
-    console.groupEnd();
-}
-export default lessons1; 
--- lessons/scripts.js
+++ lessons/scripts.js
@@ -1,2 +1,4 @@
-import lessons1 from './lesson1';
+import lessons1 from './lesson1/lesson1';
+import "regenerator-runtime/runtime";
+
 lessons1();
